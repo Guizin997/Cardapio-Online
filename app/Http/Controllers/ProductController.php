e936e67ce8396bc51c4e431603e534cf70e6df82
Guizin997 <guiguirussas@gmail.com>
2025-06-23 06:56:24 -0300

    crud dos produtos

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'image' => 'required|string',
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->noContent();
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,14 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
-
 {
-    protected $fillable = [
-        'name',
-        'price',
-        'description',
-        'image',
-    ];
-
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'image'];
 }
