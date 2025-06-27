8f55f7782236447c4f867f3f833fedcb27cd5fc6
Guizin997 <guiguirussas@gmail.com>
2025-06-26 21:43:08 -0300

    correção de bugs ao registrar usuário

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('userName');
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 // Rotas de autenticação
 Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'store']);
+Route::post('/logout', [AuthController::class, 'destroy'])->middleware('auth:sanctum');
 
 // Rotas para produtos
 Route::apiResource('products', ProductController::class);
